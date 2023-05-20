7d1490e178046fb61cb40544ec50579efe6edb5b
heonrim <92150496+heonrim@users.noreply.github.com>
2023-05-20 13:01:28 +0800

    Initial Commit

---
+++ GraduationProject/GraduationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraduationProjectApp.swift
+//  GraduationProject
+//
+//  Created by heonrim on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GraduationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
